96d357e92e037f918e1ea9e0cda60fca07bac5e4
matt <matt@weiapp.com>
2016-11-29 14:27:59 +0800

    [feature] - add v-tooltip

---
+++ src/directives/tooltip.js
@@ -0,0 +1,18 @@
+export default {
+	bind(el, {value, modifiers}) {
+		if (value) return
+		el.setAttribute('j-tooltip', value)
+		el.classList.add('j-tooltip')
+		if (modifiers.inline) {
+			el.classList.add('flex', 'inline')
+		}
+	},
+	update(el, {value, oldValue}) {
+		if (value !== oldValue) {
+			el.setAttribute('j-tooltip', value)
+		}
+	},
+	unbind(el) {
+		el.removeAttribute('j-tooltip')
+	}
+}
